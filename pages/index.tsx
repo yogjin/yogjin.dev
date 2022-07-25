92f96e1b1bd34c87140d34d381ac77df6681e13b
yogjin <youngjin988@gmail.com>
2022-07-25 23:03:43 +0900

    feat: Add Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,29 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <div className={`text-blue-500 text-3xl`}>Home</div>;
+  return (
+    <div>
+      <div className={`relative`}>
+        <Image
+          src="/home.jpeg"
+          alt="home"
+          width={'100%'}
+          height={40}
+          layout={`responsive`}
+          objectFit="cover"
+          className={`rounded-3xl`}
+        />
+        <span
+          className={`absolute top-32 text-4xl font-semibold text-white mx-6`}
+        >
+          Youngjin Park
+        </span>
+        <span className={`absolute top-44 text-xl font-normal text-white mx-6`}>
+          Frontend Developer who likes playing guitar
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
