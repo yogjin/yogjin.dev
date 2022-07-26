ef1295dd2cf04f8e1690e20824fb118204e1f4cb
yogjin <youngjin988@gmail.com>
2022-07-26 17:24:14 +0900

    feat!: Add [postId].tsx (blog post content)

---
+++ pages/posts/[postId].tsx
@@ -0,0 +1,37 @@
+import { getPostData } from '../../lib/posts';
+import { getStaticPropsParams, PostMetaData } from '../../interfaces/post';
+import { FC } from 'react';
+import Date from '../../components/date';
+
+interface PostProps extends PostMetaData {}
+
+const Post: FC<PostProps> = ({ postId, contentHtml, title, date }) => {
+  return (
+    <>
+      <article>
+        <h1>{title}</h1>
+        <div className={`mb-4`}>
+          <Date dateString={date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </article>
+    </>
+  );
+};
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { postId: 'first' } }],
+    fallback: false,
+  };
+}
+export async function getStaticProps({ params }: getStaticPropsParams) {
+  const { postId, contentHtml, title, date }: PostMetaData = await getPostData(
+    params.postId
+  );
+  return {
+    props: { postId, contentHtml, title, date },
+  };
+}
+
+export default Post;
