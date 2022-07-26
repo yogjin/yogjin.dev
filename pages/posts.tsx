38504fded4cafeb7d86972c371a0c26d32c419ae
yogjin <youngjin988@gmail.com>
2022-07-26 23:30:16 +0900

    style: overall posts page

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -7,15 +7,19 @@ const Posts = ({
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
-      <div className={`text-blue-500 text-3xl`}>Posts</div>
-      {postsData.map((postData) => (
-        <Link href={`/posts/${postData.postId}`} key={postData.postId}>
-          <a className={`mt-6`}>
-            <div className={`text-2xl font-medium`}>{postData.title}</div>
-            <div className={`text-xl my-1`}>{postData.date}</div>
-          </a>
-        </Link>
-      ))}
+      <div className={`text-blue-500 text-6xl my-10 pb-6 border-b-4`}>
+        Posts
+      </div>
+      <div className={'flex flex-col'}>
+        {postsData.map((postData) => (
+          <Link href={`/posts/${postData.postId}`} key={postData.postId}>
+            <a className={`my-6`}>
+              <div className={`text-3xl font-medium`}>{postData.title}</div>
+              <div className={`text-xl my-2`}>{postData.date}</div>
+            </a>
+          </Link>
+        ))}
+      </div>
     </>
   );
 };
