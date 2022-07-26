dbf403590fb1880921e6ef96c1e73cea469d944a
yogjin <youngjin988@gmail.com>
2022-07-26 18:00:48 +0900

    refactor(Nav): change blog to posts

--- pages/blog.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next';
-
-const Blog: NextPage = () => {
-  return <div className={`text-blue-500 text-3xl`}>Blog</div>;
-};
-
-export default Blog;
---
+++ pages/posts.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Posts: NextPage = () => {
+  return <div className={`text-blue-500 text-3xl`}>Posts</div>;
+};
+
+export default Posts;
