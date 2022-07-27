ae35c5fea4cacd33fcadc1996431ad21c951d5f0
yogjin <youngjin988@gmail.com>
2022-07-27 20:27:40 +0900

    style: set <code/>'style like notion's

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,29 @@ module.exports = {
       fontFamily: {
         logo: ['Calligraffitti'],
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#333',
+            code: {
+              // inline code: clone notion's style
+              color: '#EB5756',
+              backgroundColor: '#EDEDEB',
+              padding: '0.2em',
+              borderRadius: '0.2em',
+              fontWeight: '400',
+            },
+            'code::before': {
+              content: '""',
+              paddingLeft: '0.1em',
+            },
+            'code::after': {
+              content: '""',
+              paddingRight: '0.1em',
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
