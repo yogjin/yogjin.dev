24f4bd8e77e6efce641eb2168738b9b0fc4e77cc
yogjin <youngjin988@gmail.com>
2022-07-27 11:13:32 +0900

    style: Set header into sticky

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,18 +6,22 @@ import Image from 'next/image';
 interface LayoutProps {}
 const Layout = ({ children }: PropsWithChildren<LayoutProps>) => {
   return (
-    <div className={`w-full flex flex-col items-center p-1`}>
+    <div className={`w-full flex flex-col items-center`}>
       <Head>
         <title>yogjin.dev</title>
       </Head>
       <header
-        className={`w-full max-w-3xl flex justify-between items-center my-2`}
+        className={`flex flex-col w-full justify-center items-center bg-white sticky top-0  h-[4.3rem]`}
       >
-        <Nav />
-        <div className={`flex items-center`}>
-          <span className={`mx-2 text-2xl font-black font-logo`}>
-            yogjin.dev
-          </span>
+        <div
+          className={`w-full max-w-3xl flex justify-between items-center my-2`}
+        >
+          <Nav />
+          <div className={`flex items-center`}>
+            <span className={`mx-2 text-2xl font-black font-logo`}>
+              yogjin.dev
+            </span>
+          </div>
         </div>
       </header>
       <main className={`w-full max-w-5xl`}>{children}</main>
