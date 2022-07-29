af69a709df9dd1c2980199dcb15b3d9cc51f3d9d
yogjin <youngjin988@gmail.com>
2022-07-30 00:35:35 +0900

    feat: Add table of contents

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -1,10 +1,39 @@
+import { FC } from 'react';
+import useHeadingsData, { HeadElement } from '../hooks/useHeadingsData';
+
+interface HeadingsProps {
+  headings: HeadElement[];
+}
+const Headings: FC<HeadingsProps> = ({ headings }) => (
+  <ul>
+    {headings.map((heading) => (
+      <li key={heading.id}>
+        <a href={`#${heading.id}`}>{heading.title}</a>
+        {heading.items.length > 0 && (
+          <ul>
+            {heading.items.map((child) => (
+              <li key={child.id}>
+                <a href={`#${child.id}`} className={`ml-4`}>
+                  {child.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </li>
+    ))}
+  </ul>
+);
+
 const TableOfContents = () => {
+  const nestedHeadings: HeadElement[] = useHeadingsData();
+
   return (
     <nav
       className={`fixed top-44 right-44 border-l-2 pl-2`}
       aria-label="Table of contents"
     >
-      Hello world!
+      <Headings headings={nestedHeadings} />
     </nav>
   );
 };
