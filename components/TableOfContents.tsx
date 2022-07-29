1e63d35bbc66f2500357f9b63dd22e6945e45fa3
yogjin <youngjin988@gmail.com>
2022-07-30 01:08:59 +0900

    style(TableOfContents): color & size changes when clicked

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -1,36 +1,57 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import useHeadingsData, { HeadElement } from '../hooks/useHeadingsData';
 
 interface HeadingsProps {
   headings: HeadElement[];
 }
-const Headings: FC<HeadingsProps> = ({ headings }) => (
-  <ul>
-    {headings.map((heading) => (
-      <li key={heading.id}>
-        <a href={`#${heading.id}`}>{heading.title}</a>
-        {heading.items.length > 0 && (
-          <ul>
-            {heading.items.map((child) => (
-              <li key={child.id}>
-                <a href={`#${child.id}`} className={`ml-4`}>
-                  {child.title}
-                </a>
-              </li>
-            ))}
-          </ul>
-        )}
-      </li>
-    ))}
-  </ul>
-);
+const Headings: FC<HeadingsProps> = ({ headings }) => {
+  const [selected, setSelected] = useState<string>();
+  return (
+    <ul>
+      {headings.map((heading) => (
+        <li key={heading.id}>
+          <a
+            href={`#${heading.id}`}
+            className={`text-[#ACACAC] hover:text-black transition-all duration-300 inline-block ${
+              selected === heading.id && `text-black scale-110`
+            }`}
+            onClick={() => {
+              setSelected(heading.id);
+            }}
+          >
+            {heading.title}
+          </a>
+          {heading.items.length > 0 && (
+            <ul>
+              {heading.items.map((child) => (
+                <li key={child.id}>
+                  <a
+                    href={`#${child.id}`}
+                    className={`ml-4 text-[#ACACAC] hover:text-black transition-all duration-300 inline-block ${
+                      selected === child.id && `text-black scale-110`
+                    }`}
+                    onClick={() => {
+                      setSelected(child.id);
+                    }}
+                  >
+                    {child.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const TableOfContents = () => {
   const nestedHeadings: HeadElement[] = useHeadingsData();
 
   return (
     <nav
-      className={`fixed top-44 right-44 border-l-2 pl-2`}
+      className={`fixed top-44 right-44 border-l-2 pl-4`}
       aria-label="Table of contents"
     >
       <Headings headings={nestedHeadings} />
