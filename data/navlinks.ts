14a00e7dcc786ae18d30632b125ce0b3b1e365b3
yogjin <youngjin988@gmail.com>
2022-08-25 00:21:20 +0900

    refactor: 카테고리 nav 타입을 직접 명시하는 것으로 수정
    원래 src/posts에 있는 디렉토리를 가져와서 카테고리 nav에 사용했는데, 한글로 라우팅이 안되는 문제 때문에 이런 방식으로 변경함

--- data/navlinks.ts
+++ data/navlinks.ts
@@ -1,6 +1,9 @@
-const navlinks: { title: string; link: string }[] = [
+export const navlinks: { title: string; link: string }[] = [
   { title: 'Home', link: '/' },
   { title: 'Posts', link: '/posts' },
 ];
 
-export default navlinks;
+export const categoryNavlinks: { title: string; link: string }[] = [
+  { title: 'jsdeepdive', link: '/posts/jsdeepdive' },
+  { title: '회고', link: '/posts/retrospect' },
+];
