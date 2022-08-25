c9271e4c112274024afb2460438ac421569a9f4f
yogjin <youngjin988@gmail.com>
2022-08-25 15:29:05 +0900

    style: 선택된 category 스타일 변경

--- components/CategoryNav.tsx
+++ components/CategoryNav.tsx
@@ -2,9 +2,10 @@ import Link from 'next/link';
 import { categoryNavlinks } from '../data/navlinks';
 
 interface Props {
+  selectedCategory: any;
   onClickCategory: any;
 }
-const CategoryNav = ({ onClickCategory }: Props) => {
+const CategoryNav = ({ selectedCategory, onClickCategory }: Props) => {
   return (
     <div className={`text-blue-500 text-xl font-medium mt-10 pb-1 border-b-2`}>
       <nav className="flex space-x-4">
@@ -12,7 +13,13 @@ const CategoryNav = ({ onClickCategory }: Props) => {
           <button
             key={categoryNavlink.title}
             onClick={() => onClickCategory(categoryNavlink.category)}
-            className={`rounded-lg px-3 py-2 text-base font-normal text-blue-600 hover:bg-slate-200  bg-slate-100 transition duration-300 cursor-pointer`}
+            className={`rounded-lg px-3 py-2 text-base font-normal transition duration-300 cursor-pointer 
+              ${
+                categoryNavlink.category === selectedCategory
+                  ? `text-slate-100 bg-blue-600 hover:bg-blue-700`
+                  : `text-blue-600 bg-slate-100 hover:bg-slate-200`
+              }
+            `}
           >
             {categoryNavlink.title}
           </button>
