1ae2b394176e0c7ea4a6978f510fc8ea0287661b
yogjin <youngjin988@gmail.com>
2022-08-25 22:51:30 +0900

    feat: post를 카드 섹션으로 보여줌

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,10 +1,6 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
-import { FC } from 'react';
 import { PostMetaData } from '../interfaces/post';
-import Date from './date';
-import cardImage from '/public/thumbnails/데이터타입.png';
+import PostCard from './PostCard';
 
 interface Props {
   postsData: PostMetaData[];
@@ -14,34 +10,14 @@ interface Props {
 const Posts = ({ postsData, selectedCategory }: Props) => {
   return (
     <div className={'grid grid-cols-2 gap-4 pt-2'}>
-      {postsData.map(
-        (postData) =>
+      {postsData.map((postData) => {
+        return (
           (selectedCategory === 'All' ||
             selectedCategory === postData.category) && (
-            <section
-              className={`border border-slate-300 rounded hover:shadow-xl ease-out duration-300`}
-            >
-              <Link href={`/posts/${postData.postId}`} key={postData.postId}>
-                <a className={`mt-6`}>
-                  <Image
-                    className={`hover:scale-110 ease-out duration-300`}
-                    src={cardImage}
-                    alt="test"
-                    width={100}
-                    height={60}
-                    layout="responsive"
-                  ></Image>
-                  <div className={`p-2`}>
-                    <div className={`text-xl font-medium`}>
-                      {postData.title}
-                    </div>
-                    <Date dateString={postData.date} />
-                  </div>
-                </a>
-              </Link>
-            </section>
+            <PostCard postData={postData} />
           )
-      )}
+        );
+      })}
     </div>
   );
 };
