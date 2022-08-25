88598c8bfb0df364c07dfef155884543efcc4fff
Youngjin Park <youngjin988@gmail.com>
2022-08-26 02:30:34 +0900

    feat: 포스팅 카드섹션 추가 (#10)
    * feat: 포스팅 목록을 카드섹션 형태로 변경
    
    * refactor: update thumbnails
    
    * feat(PostMetaData):  썸네일의 이미지 경로인 thumbnail 프로퍼티 추가
    
    * feat: post를 카드 섹션으로 보여줌
    
    * refactor: Posts -> PostCards 컴포넌트 이름 변경

---
+++ components/Posts.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { PostMetaData } from '../interfaces/post';
+import PostCard from './PostCard';
+
+interface Props {
+  postsData: PostMetaData[];
+  selectedCategory: string;
+}
+
+const PostCards = ({ postsData, selectedCategory }: Props) => {
+  return (
+    <div className={'grid grid-cols-2 gap-4 pt-2'}>
+      {postsData.map((postData) => {
+        return (
+          (selectedCategory === 'All' ||
+            selectedCategory === postData.category) && (
+            <PostCard postData={postData} />
+          )
+        );
+      })}
+    </div>
+  );
+};
+
+export default PostCards;
