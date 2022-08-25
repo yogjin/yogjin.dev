59e2f8fa9982193b9b1fd255a457966cef800337
yogjin <youngjin988@gmail.com>
2022-08-25 14:17:49 +0900

    feat!: category 별로 포스팅 분류 기능 추가

--- components/CategoryNav.tsx
+++ components/CategoryNav.tsx
@@ -1,18 +1,21 @@
 import Link from 'next/link';
 import { categoryNavlinks } from '../data/navlinks';
 
-const CategoryNav = () => {
+interface Props {
+  onClickCategory: any;
+}
+const CategoryNav = ({ onClickCategory }: Props) => {
   return (
     <div className={`text-blue-500 text-xl font-medium mt-10 pb-1 border-b-2`}>
       <nav className="flex space-x-4">
-        {categoryNavlinks.map((category) => (
-          <Link href={category.link} key={category.title}>
-            <a
-              className={`rounded-lg px-1 py-2 text-base font-normal text-blue-600 hover:bg-slate-100 hover:text-blue-700`}
-            >
-              {category.title}
-            </a>
-          </Link>
+        {categoryNavlinks.map((categoryNavlink) => (
+          <button
+            key={categoryNavlink.title}
+            onClick={() => onClickCategory(categoryNavlink.category)}
+            className={`rounded-lg px-3 py-2 text-base font-normal text-blue-600 hover:bg-slate-200  bg-slate-100 transition duration-300 cursor-pointer`}
+          >
+            {categoryNavlink.title}
+          </button>
         ))}
       </nav>
     </div>
