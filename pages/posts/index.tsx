9958f831d5d7a4d60a194ae53393c47f3e5c9eb4
yogjin <youngjin988@gmail.com>
2022-08-25 14:17:49 +0900

    feat!: category 별로 포스팅 분류 기능 추가

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import type { InferGetStaticPropsType, NextPage } from 'next';
 import Link from 'next/link';
+import { useState } from 'react';
 import CategoryNav from '../../components/CategoryNav';
 import Date from '../../components/date';
 import {
@@ -12,19 +13,29 @@ const Posts = ({
   postsData,
   postsCategory,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [selectedCategory, setSelectedCategory] = useState<string>('All');
+
+  function handleClickCategory(clicked: string) {
+    console.log(clicked);
+    setSelectedCategory(clicked);
+  }
   return (
     <>
-      <CategoryNav />
-      {/* <div className={'flex flex-col'}>
-        {postsData.map((postData) => (
-          <Link href={`/posts/${postData.postId}`} key={postData.postId}>
-            <a className={`mt-6`}>
-              <div className={`text-2xl font-medium`}>{postData.title}</div>
-              <Date dateString={postData.date} />
-            </a>
-          </Link>
-        ))}
-      </div> */}
+      <CategoryNav onClickCategory={handleClickCategory} />
+      <div className={'flex flex-col'}>
+        {postsData.map(
+          (postData) =>
+            (selectedCategory === 'All' ||
+              selectedCategory === postData.category) && (
+              <Link href={`/posts/${postData.postId}`} key={postData.postId}>
+                <a className={`mt-6`}>
+                  <div className={`text-2xl font-medium`}>{postData.title}</div>
+                  <Date dateString={postData.date} />
+                </a>
+              </Link>
+            )
+        )}
+      </div>
     </>
   );
 };
