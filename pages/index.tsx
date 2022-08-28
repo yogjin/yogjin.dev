83ba7d2dfccc8cb7dd50e614662d1a634c50dca1
Youngjin Park <youngjin988@gmail.com>
2022-08-28 19:49:38 +0900

    feat: google search console에 등록하기 위한 meta 태그 설정 (#11)
    * post: js-타입 변환과 단축 평가
    
    * feat: 구글 서치 콘솔 등록을 위한 meta 태그 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,32 +9,40 @@ const Home = ({
   postDatas,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <div>
-      <div className={`relative`}>
-        <Image
-          src="/home.jpeg"
-          alt="home"
-          width={'100%'}
-          height={40}
-          layout={`responsive`}
-          objectFit="cover"
-          className={`rounded-3xl`}
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION}
         />
-        <div
-          className={`flex flex-col absolute left-2 top-2 bg-black bg-opacity-90 rounded-xl py-8 m-2`}
-        >
-          <span className={`text-4xl font-semibold text-white mx-6`}>
-            Youngjin Park
-          </span>
-          <span className={`text-base font-normal text-white mx-6`}>
-            Frontend Developer who likes playing guitar
-          </span>
+      </Head>
+      <div>
+        <div className={`relative`}>
+          <Image
+            src="/home.jpeg"
+            alt="home"
+            width={'100%'}
+            height={40}
+            layout={`responsive`}
+            objectFit="cover"
+            className={`rounded-3xl`}
+          />
+          <div
+            className={`flex flex-col absolute left-2 top-2 bg-black bg-opacity-90 rounded-xl py-8 m-2`}
+          >
+            <span className={`text-4xl font-semibold text-white mx-6`}>
+              Youngjin Park
+            </span>
+            <span className={`text-base font-normal text-white mx-6`}>
+              Frontend Developer who likes playing guitar
+            </span>
+          </div>
+        </div>
+        <div>
+          <RecentPosts postDatas={postDatas} />
         </div>
       </div>
-      <div>
-        <RecentPosts postDatas={postDatas} />
-      </div>
-    </div>
+    </>
   );
 };
 export function getStaticProps() {
