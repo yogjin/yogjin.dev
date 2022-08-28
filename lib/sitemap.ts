f66aa6429388b41f69f2293d9919c82a6d7d1a0e
yogjin <youngjin988@gmail.com>
2022-08-29 08:29:53 +0900

    fix: sitemap 수정

--- lib/sitemap.ts
+++ lib/sitemap.ts
@@ -26,15 +26,14 @@ export function generateSitemap(allPosts: PostMetaData[]) {
     .join('');
 
   const sitemap = `
-    <xml version="1.0" encoding="UTF-8">
+    <?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
           <loc>${BLOG_DOMAIN}</loc>
           <lastmod>${TODAY}</lastmod>
         </url>
         ${sitemapURLs}
-      </urlset>
-    </xml>`;
+      </urlset>`;
 
   const formattedSitemap = formatted(sitemap);
 
