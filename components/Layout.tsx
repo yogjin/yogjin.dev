a82fc6c1add54a4b172f59f5f94cf55852aa9ecb
yogjin <youngjin988@gmail.com>
2022-09-12 21:27:44 +0900

    style(header): padding 부여

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -21,7 +21,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <title>yogjin.dev</title>
       </Head>
       <header
-        className={`z-50 flex flex-col w-full justify-center items-center bg-white sticky top-0 h-[3.5rem] ${
+        className={`z-50 flex flex-col w-full justify-center items-center bg-white sticky top-0 h-[3.5rem] p-4 ${
           scrollDirection === 'down' ? '-top-[3.5rem]' : 'top-0'
         } ${
           scrollLocation !== 0 && 'drop-shadow-md'
